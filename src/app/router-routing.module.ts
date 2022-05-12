36e0360ff259e3bae490eae25f0cfdf1c4bcac56
felixhebles <dam2felixheblesjimenez@iesantoniogala.es>
2022-05-12 14:36:13 +0200

    login merge

--- src/app/router-routing.module.ts
+++ src/app/router-routing.module.ts
@@ -1,10 +1,13 @@
+import { HtmlParser } from '@angular/compiler';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
 
-  {path : 'login', component : LoginComponent}
+  {path : 'login', component : LoginComponent},
+  {path : 'home', component : HomeComponent}
 ];
 
 @NgModule({
